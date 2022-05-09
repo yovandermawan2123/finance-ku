495f767353793d23c6f5251d4b4d63d0464bc623
yovandermawan2123 <yovanwota@gmail.com>
2022-05-10 01:05:26 +0700

    Membuat Validasi Data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,51 +7,19 @@
       
     <div class="px-2 py-2  rounded" style="background-color: rgb(252, 250, 250)">
 
+      @if ($finances->count())
+
       <h5>Sisa Uang : {{ rupiah($revenue - $expense) }}</h5>
       <div class="progress bg-grey mb-3">
         <div class="progress-bar bg-warning" role="progressbar" style="width: {{ persentase($revenue, $expense) }};" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" >{{ persentase($revenue, $expense) }}</div>
       </div>
       <hr class="text-grey">
 
-{{-- 
 
-          <table class="bg-white table table-striped table-hover">
-            <thead>
-              <tr>
-                <th scope="col">No</th>
-                <th scope="col">Judul</th>
-                <th scope="col">Jumlah</th>
-                <th scope="col">Tanggal</th>
-                <th scope="col">Tipe</th>
-                <th scope="col">Aksi</th>
-                
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($finances as $finance)
-              <tr>
-                
-                <td>{{ $loop->iteration}}</td>
-                <td>{{ $finance->title }}</td>
-                <td>{{ $finance->amount }}</td>
-                <td>{{ $finance->date }}</td>
-                <td>{{ $finance->type }}</td>
-                <td>
-                  <button type="button" class="editbtn" value="{{ $finance->id }}">Edit</button>
-                
-                  <form action="/home/{{ $finance->id }}" method="POST" class="d-inline">
-                    @method('DELETE')
-                    @csrf
-                  <button class="border-0 badge bg-danger" onclick="return confirm('Are you sure?')"><i class=" bi bi-trash text-white"></i></td></button>
-              
-                </form>
-                </tr>
-              @endforeach
-            
-              
-            </tbody>
-          </table> --}}
           
+    
+              
+
           @foreach ($finances as $finance)
 
         
@@ -89,6 +57,75 @@
           <div class="d-flex justify-content-center">
           {{ $finances->links() }}
         </div>
+          
+
+         {{-- Edit modal --}}
+         <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Pencatatan Keuangan</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                
+                <form action="/home/{{ $finance->id }}" method="POST" >
+                  @method('put')
+                  @csrf
+                  <input type="hidden" id="finance_id">
+                  <div class="mb-3">
+                    <label for="judul" class="col-form-label">Judul:</label>
+                    <input type="text" name="title" id="title" class="form-control" id="judul">
+                  </div>
+                  <div class="mb-3">
+                    <label for="jumlah" class="col-form-label">Jumlah:</label>
+                    <input type="text" name="amount" id="amount" class="form-control" id="jumlah">
+                  </div>
+                 
+                  <div class="mb-3">
+                    <label for="startDate" class="col-form-label">Tanggal dan waktu:</label>
+                  <input id="startDate" class="form-control" name="date" id="date" value="{{Carbon\Carbon::now()->format('Y-m-d')."T".Carbon\Carbon::now()->format('H:i')}}" type="datetime-local" />
+                  </div>
+
+                  <div class="mb-3">
+                  <label for="tipe" class="col-form-label">Tipe:</label>
+                  <select class="form-select " name="type" id="type"  aria-label="Default select example">
+                    <option selected>-- Pilih Tipe --</option>
+                    <option value="pemasukan">Pemasukan</option>
+                    <option value="pengeluaran">Pengeluaran</option>
+                    
+                  </select>
+                </div>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" name="submit" class="btn btn-primary">Update Pencatatan</button>
+                </form>
+              </div>
+              <div class="modal-footer">
+              </div>
+            </div>
+          </div>
+        </div>
+        {{-- END EDIT MODAL --}}
+
+
+
+          @else
+         
+          <h5>Sisa Uang : 0</h5>
+          <div class="progress bg-grey mb-3">
+            <div class="progress-bar bg-warning" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" >0%</div>
+          </div>
+          <hr class="text-grey">
+    
+    
+              
+        
+                  
+    
+             <h5>Data Tidak ditemukan</h5>
+          
+          @endif
+         
 
         </div>
        {{-- Add modal --}}
@@ -136,52 +173,7 @@
           </div>
           {{-- END ADD MODAL --}}
 
-          {{-- Edit modal --}}
-          <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Edit Pencatatan Keuangan</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <form action="/home/{{ $finance->id }}" method="POST" >
-                    @method('put')
-                    @csrf
-                    <input type="hidden" id="finance_id">
-                    <div class="mb-3">
-                      <label for="judul" class="col-form-label">Judul:</label>
-                      <input type="text" name="title" id="title" class="form-control" id="judul">
-                    </div>
-                    <div class="mb-3">
-                      <label for="jumlah" class="col-form-label">Jumlah:</label>
-                      <input type="text" name="amount" id="amount" class="form-control" id="jumlah">
-                    </div>
-                   
-                    <div class="mb-3">
-                      <label for="startDate" class="col-form-label">Tanggal dan waktu:</label>
-                    <input id="startDate" class="form-control" name="date" id="date" value="{{Carbon\Carbon::now()->format('Y-m-d')."T".Carbon\Carbon::now()->format('H:i')}}" type="datetime-local" />
-                    </div>
-
-                    <div class="mb-3">
-                    <label for="tipe" class="col-form-label">Tipe:</label>
-                    <select class="form-select " name="type" id="type"  aria-label="Default select example">
-                      <option selected>-- Pilih Tipe --</option>
-                      <option value="pemasukan">Pemasukan</option>
-                      <option value="pengeluaran">Pengeluaran</option>
-                      
-                    </select>
-                  </div>
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="submit" name="submit" class="btn btn-primary">Update Pencatatan</button>
-                  </form>
-                </div>
-                <div class="modal-footer">
-                </div>
-              </div>
-            </div>
-          </div>
-          {{-- END EDIT MODAL --}}
+         
     </div>
 </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <title>Hello, world!</title>
+    <title>Finance-Ku | Pencatatan Keuangan Digital</title>
     <style>
       .card-finances {
         -webkit-box-shadow: inset 10px -1px 44px -35px rgba(0,0,0,0.75);
